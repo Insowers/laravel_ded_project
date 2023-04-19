b1e947ff49f57bde67d28dea85ece9a18b77e953
Insowers <francesco.cadderi@gmail.com.com>
2023-04-19 12:06:16 +0200

    query a caso

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -11,7 +11,24 @@ class MonsterController extends Controller
 {
     public function index()
         {
-            $monsters = [Monster::all(), Weapon::all(), Spell::all()];
-            return response()->json($monsters);
+            /* $monsters = [Monster::all(), Weapon::all(), Spell::all()];
+            return response()->json($monsters); */
+
+            /* $monsters_pivot = Monster::all();
+            foreach ($monsters_pivot->weapons as $weapon) {
+                return response()->json($weapon->pivot->quantities);
+            } */
+
+            /* Monster::where('quantities', 1)
+                ->whereHas('weapon', function($q) {
+                    $q->where('id_weapon', 1);
+                })
+                ->get(); */
+
+            /* $users = DB::table('users')
+            ->join('contacts', 'users.id', '=', 'contacts.user_id')
+            ->join('orders', 'users.id', '=', 'orders.user_id')
+            ->select('users.*', 'contacts.phone', 'orders.price')
+            ->get(); */
         }
 }
